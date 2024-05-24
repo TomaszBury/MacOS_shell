a90d220a94178fe36e9a3d5b7f840cd6394453c2
Tomasz Bury <32302797+TomaszBury@users.noreply.github.com>
2024-05-24 17:39:43 +0200

    Chrome Unistaller

---
+++ First_try/chrome_uninstaller.sh
@@ -0,0 +1,37 @@
+# MIT License
+
+# Copyright (c) 2020 Wendy Liga
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# 3 copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+rm -rf /Applications/Google\ Chrome.app/
+rm /Library/LaunchAgents/com.google.keystone*
+rm -rf /Library/Application\ Support/Google/Chrome
+rm ~/Library/Preferences/com.google.Chrome*
+
+rm -rf ~/Applications/Chrome\ Apps.localized/
+rm ~/Library/LaunchAgents/com.google.keystone*
+rm -rf ~/Library/Application\ Support/Google/Chrome
+rm ~/Library/Preferences/com.google.Chrome*
+rm -rf ~/Library/Application\ Support/CrashReporter/Google\ Chrome
+rm ~/Library/Preferences/Google\ Chrome*
+rm -r ~/Library/Caches/com.google.Chrome*
+rm -r ~/Library/Saved\ Application\ State/com.google.Chrome.savedState/
+rm ~/Library/Google/GoogleSoftwareUpdate/Actives/com.google.Chrome
+rm ~/Library/Google/Google\ Chrome*
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # My Fav commands.
+
+
+[Chrome Unistaller](https://github.com/wendyliga/mac_chrome_uninstaller)
